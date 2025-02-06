f16163af175e98fbf5051e3c4c08b15d2acda1e8
Ruthzxc17 <137496574+Ruthzxc17@users.noreply.github.com>
2025-02-06 13:01:23 +0800

    ScrollView

---
+++ Activity1/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Image, ScrollView, Text} from 'react-native';
+
+const logo = {
+  uri: 'https://reactnative.dev/img/tiny_logo.png',
+  width: 64,
+  height: 64,
+};
+
+const App = () => (
+  <ScrollView>
+    <Text style={{fontSize: 96}}>Scroll Melrose MelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelroseMelrose </Text>
+    
+  </ScrollView>
+);
+
+export default App;
